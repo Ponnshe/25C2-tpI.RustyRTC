2d999fe33d718844b7b4d82ed32cd9891d1246e2
Tom Pinargote <tpinargote@fi.uba.ar>
2025-10-05 20:09:35 -0300

    tests: adding unit test for attribute module in SDP (#5)

--- src/sdp/attribute.rs
+++ src/sdp/attribute.rs
@@ -55,3 +55,90 @@ impl Attribute {
         self.value = value.into();
     }
 }
+#[cfg(test)]
+mod tests {
+    #![allow(clippy::unwrap_used, clippy::expect_used)]
+    use super::Attribute;
+
+    #[test]
+    fn new_with_some_string_sets_fields() {
+        let a = Attribute::new("rtpmap", Some(String::from("111 opus/48000/2")));
+        assert_eq!(a.key(), "rtpmap");
+        assert_eq!(a.value(), Some("111 opus/48000/2"));
+    }
+
+    #[test]
+    fn new_with_some_string_alt() {
+        // Same idea as the previous test, using to_string()
+        let a = Attribute::new("fmtp", Some("111 minptime=10;usedtx=1".to_string()));
+        assert_eq!(a.key(), "fmtp");
+        assert_eq!(a.value(), Some("111 minptime=10;usedtx=1"));
+    }
+
+    #[test]
+    fn new_with_none_sets_value_none() {
+        let a = Attribute::new("rtcp-mux", None::<String>);
+        assert_eq!(a.key(), "rtcp-mux");
+        assert_eq!(a.value(), None);
+    }
+
+    #[test]
+    fn new_blank_defaults() {
+        let a = Attribute::new_blank();
+        assert_eq!(a.key(), "");
+        assert_eq!(a.value(), None);
+    }
+
+    #[test]
+    fn setters_update_key_and_value() {
+        let mut a = Attribute::new_blank();
+
+        a.set_key("ice-ufrag");
+        a.set_value(Some("abcd".to_string()));
+        assert_eq!(a.key(), "ice-ufrag");
+        assert_eq!(a.value(), Some("abcd"));
+
+        // Limpiar valor a None
+        a.set_value(None::<String>);
+        assert_eq!(a.value(), None);
+
+        // Acepta valor vacío explícito (distinto de None)
+        a.set_value(Some(String::new()));
+        assert_eq!(a.value(), Some(""));
+
+        // Cambiar clave nuevamente
+        a.set_key(String::from("ice-pwd"));
+        assert_eq!(a.key(), "ice-pwd");
+    }
+
+    #[test]
+    fn allows_empty_whitespace_and_weird_keys() {
+        let mut a = Attribute::new_blank();
+
+        a.set_key("");
+        assert_eq!(a.key(), "");
+
+        a.set_key("  ");
+        assert_eq!(a.key(), "  ");
+
+        a.set_key("x-foo:bar;baz");
+        assert_eq!(a.key(), "x-foo:bar;baz");
+    }
+
+    #[test]
+    fn many_updates_last_write_wins() {
+        let mut a = Attribute::new("k0", Some("v0".to_string()));
+
+        for i in 1..10_000 {
+            a.set_key(format!("k{i}"));
+            a.set_value(Some(format!("v{i}")));
+        }
+
+        assert_eq!(a.key(), "k9999");
+        assert_eq!(a.value(), Some("v9999"));
+
+        // Y limpiar al final
+        a.set_value(None::<String>);
+        assert_eq!(a.value(), None);
+    }
+}
