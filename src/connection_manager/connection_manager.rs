a16c137e6b41e8d7599de3eea45393ae8fba3457
Tom Pinargote <tpinargote@fi.uba.ar>
2025-10-13 00:43:27 -0300

    (feat): defining const values to be used for mocked data (#26)

--- src/connection_manager/connection_manager.rs
+++ src/connection_manager/connection_manager.rs
@@ -10,3 +10,13 @@ use crate::sdp::origin::Origin as SDPOrigin;
 use crate::sdp::port_spec::PortSpec as SDPPortSpec;
 use crate::sdp::sdpc::Sdp;
 use crate::sdp::time_desc::TimeDesc as SDPTimeDesc;
+
+const DEFAULT_PORT: u16 = 9;
+const DEAFULT_PROTO: &str = "UDP/TLS/RTP/SAVPF";
+const DEFAULT_FMT: &str = "99";
+const DEFAULT_NET_TYPE: &str = "IN";
+const DEFAULT_ADDR_TYPE: SDPAddrType = SDPAddrType::IP4;
+const DEFAULT_CONN_ADDR: &str = "0.0.0.0";
+const DEFAULT_CODEC: &str = "VP8 90000";
+const DEAULT_MEDIA_KIND: SDPMediaKind = SDPMediaKind::Application;
+
