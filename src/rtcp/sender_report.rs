c087bea25fd1aa232cde9899ee82154dce193bdf
Tom Pinargote <tpinargote@fi.uba.ar>
2025-11-30 19:01:36 -0300

    (fix): sender_report repeat_n

--- src/rtcp/sender_report.rs
+++ src/rtcp/sender_report.rs
@@ -34,7 +34,7 @@ impl RtcpPacketType for SenderReport {
         // Pad to 32-bit
         let pad = (4 - (out.len() - start) % 4) % 4;
         if pad != 0 {
-            out.extend(std::iter::repeat(0u8).take(pad));
+            out.extend(std::iter::repeat_n(0u8, pad));
         }
 
         // fix length
